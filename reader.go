e6736cf34cd53c820afd555c07cefca9157e9977
Sebastiaan Klippert <sebastiaan@garagesoftware.nl>
2015-09-16 01:49:28 +0200

    implemented record reading and field conversions

--- reader.go
+++ reader.go
@@ -7,19 +7,28 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
+)
+
+var (
+	ErrEOF        = fmt.Errorf("EOF")
+	ErrBOF        = fmt.Errorf("BOF")
+	ErrIncomplete = fmt.Errorf("Incomplete record read")
 )
 
 //The main DBF struct provides all methods for reading files and embeds the file handlers
 //Only files are supported at this time
 type DBF struct {
-	Header    *DBFHeader
+	header    *DBFHeader
 	f         *os.File
-	FPTHeader *FPTHeader
+	fptheader *FPTHeader
 	fptf      *os.File //if there is an FPT file handler is used for it
 
 	fields []FieldHeader
+
+	recpointer uint32 //internal record pointer, can be moved using Skip() and GoTo()
 }
 
 //The caller is responsible for calling Close to close the file handle(s)!
@@ -41,6 +50,11 @@ func (dbf *DBF) Close() error {
 	}
 }
 
+//Returns the DBF Header struct for inspecting
+func (dbf *DBF) Header() *DBFHeader {
+	return dbf.header
+}
+
 //os FileInfo for DBF file
 func (dbf *DBF) Stat() (os.FileInfo, error) {
 	return dbf.f.Stat()
@@ -54,9 +68,19 @@ func (dbf *DBF) StatFPT() (os.FileInfo, error) {
 	return dbf.fptf.Stat()
 }
 
+//Returns the number of records
+func (dbf *DBF) NumRecords() uint32 {
+	return dbf.header.NumRec
+}
+
 //Returns all the FieldHeaders
 func (dbf *DBF) Fields() []FieldHeader {
 	return dbf.fields
+}
+
+//Returns the number of fields
+func (dbf *DBF) NumFields() uint16 {
+	return uint16(len(dbf.fields))
 }
 
 //Returnes a slice of all the fieldnames
@@ -80,14 +104,129 @@ func (dbf *DBF) FieldPos(fieldname string) int {
 	return -1
 }
 
+//Sets internal record pointer to record recno (zero based)
+//Returns ErrEOF if at EOF and positions the pointer at lastRec+1
+func (dbf *DBF) GoTo(recno uint32) error {
+	if recno > dbf.header.NumRec-1 {
+		dbf.recpointer = dbf.header.NumRec
+		return ErrEOF
+	}
+	dbf.recpointer = recno
+	return nil
+}
+
+//Adds offset to the internal record pointer
+//Returns ErrEOF if at EOF and positions the pointer at lastRec+1
+//Returns ErrBOF is recpointer would be become negative and positions the pointer at 0
+//Does not skip deleted records
+func (dbf *DBF) Skip(offset int32) error {
+	if int64(dbf.recpointer)+int64(offset) > int64(dbf.header.NumRec-1) {
+		dbf.recpointer = dbf.header.NumRec
+		return ErrEOF
+	}
+	if int64(dbf.recpointer)+int64(offset) < 0 {
+		dbf.recpointer = 0
+		return ErrBOF
+	}
+	neg := offset < 0
+	var uintoffset uint32
+	if neg {
+		uintoffset = uint32(-1 * offset)
+	} else {
+		uintoffset = uint32(offset)
+	}
+	dbf.recpointer += uintoffset
+	return nil
+}
+
+//Reads the complete record the internal record pointer is pointing to
+func (dbf *DBF) Record() (*Record, error) {
+	data, err := dbf.readRecord(dbf.recpointer)
+	if err != nil {
+		return nil, err
+	}
+	return dbf.bytesToRecord(data)
+}
+
+//Reads  the complete record number nrec
+func (dbf *DBF) RecordAt(nrec uint32) (*Record, error) {
+	data, err := dbf.readRecord(nrec)
+	if err != nil {
+		return nil, err
+	}
+	return dbf.bytesToRecord(data)
+}
+
+//Reads field nfield at the record number the internal pointer is pointing to
+func (dbf *DBF) Field(nfield uint16) (interface{}, error) {
+	return nil, nil
+}
+
+//Returns if the internal recordpointer is at EoF
+func (dbf *DBF) EOF() bool {
+	return dbf.recpointer > dbf.header.NumRec-1
+}
+
+//Returns if the internal recordpointer is at BoF (first record)
+func (dbf *DBF) BOF() bool {
+	return dbf.recpointer == 0
+}
+
+//Reads raw field data of one field at fieldpos at recordpos
+func (dbf *DBF) readField(recordpos uint32, fieldpos uint16) ([]byte, error) {
+	return nil, nil
+}
+
+//Reads raw record data of one record at recordpos
+func (dbf *DBF) readRecord(recordpos uint32) ([]byte, error) {
+	if recordpos > dbf.header.NumRec-1 {
+		return nil, ErrEOF
+	}
+	buf := make([]byte, dbf.header.RecordLen)
+	read, err := dbf.f.ReadAt(buf, int64(dbf.header.FirstRec)+(int64(recordpos)*int64(dbf.header.RecordLen)))
+	if err != nil {
+		return buf, err
+	}
+	if read != int(dbf.header.RecordLen) {
+		return buf, ErrIncomplete
+	}
+	return buf, nil
+}
+
+//Converts raw recorddata to a Record struct
+//If the data points to a memo (FPT) file this file is also read
+func (dbf *DBF) bytesToRecord(data []byte) (*Record, error) {
+	rec := new(Record)
+	rec.Deleted = data[0] == 0x20
+	rec.data = make([]interface{}, dbf.NumFields())
+
+	offset := uint8(1) //deleted flag already read
+	for i := 0; i < len(rec.data); i++ {
+		fieldinfo := dbf.fields[i]
+		//fmt.Println(fieldinfo.FieldName())
+		val, err := fieldDataToValue(data[offset:offset+fieldinfo.Len], fieldinfo.FieldType(), fieldinfo.Decimals)
+		if err != nil {
+			fmt.Println(err)
+			//return rec, err
+		}
+		rec.data[i] = val
+
+		offset += fieldinfo.Len
+	}
+	/*for _, val := range rec.data {
+		fmt.Println(val)
+	}*/
+	return rec, nil
+}
+
 //Header info from https://msdn.microsoft.com/en-us/library/st4a0s68%28VS.80%29.aspx
 type DBFHeader struct {
-	FileVersion byte     //File type, only tested for 0x30 and 0x31!
+	FileVersion byte     //File type flag
 	ModYear     uint8    //Last update year (0-99)
 	ModMonth    uint8    //Last update month
 	ModDay      uint8    //Last update day
 	NumRec      uint32   //Number of records in file
-	HeaderLen   uint16   //Position of first data record
+	FirstRec    uint16   //Position of first data record
 	RecordLen   uint16   //Length of one data record, including delete flag
 	Reserved    [16]byte //Reserved
 	TableFlags  byte     //Table flags
@@ -101,6 +240,13 @@ func (h *DBFHeader) Modified() time.Time {
 	return time.Date(2000+int(h.ModYear), time.Month(h.ModMonth), int(h.ModDay), 0, 0, 0, 0, time.Local)
 }
 
+//Returns the calculated number of fields from the header info alone (without the need to read the fieldinfo from the header)
+//This is the fastest way to determine the number of records in the file
+//Note: when OpenFile is used the fields have already been parsed so it is better to call DBF.NumFields in that case
+func (h *DBFHeader) NumFields() uint16 {
+	return uint16((h.FirstRec - 296) / 32)
+}
+
 //Header info from https://msdn.microsoft.com/en-US/library/8599s21w%28v=vs.80%29.aspx
 type FPTHeader struct {
 	NextFree  uint32  //Location of next free block
@@ -111,14 +257,14 @@ type FPTHeader struct {
 //Field subrecord structure from header
 //Header info from https://msdn.microsoft.com/en-us/library/st4a0s68%28VS.80%29.aspx
 type FieldHeader struct {
-	Name     [10]byte //Field name with a maximum of 10 characters. If less than 10, it is padded with null characters (0x00).
+	Name     [11]byte //Field name with a maximum of 10 characters. If less than 10, it is padded with null characters (0x00).
 	Type     byte     //Field type
 	Pos      uint32   //Displacement of field in record
 	Len      uint8    //Length of field (in bytes)
 	Decimals uint8    //Number of decimal places
 	Flags    byte     //Field flags
 	Next     uint32   //Value of autoincrement Next value
-	Step     uint8    //Value of autoincrement Step value
+	Step     uint16   //Value of autoincrement Step value
 	Reserved [8]byte  //Reserved
 }
 
@@ -132,6 +278,12 @@ func (f *FieldHeader) FieldName() string {
 //Fieldtype as string (length 1)
 func (f *FieldHeader) FieldType() string {
 	return string(f.Type)
+}
+
+//Record data
+type Record struct {
+	Deleted bool
+	data    []interface{}
 }
 
 //Opens a DBF file (and FPT if needed) from disk.
@@ -156,14 +308,14 @@ func OpenFile(filename string) (*DBF, error) {
 		return nil, err
 	}
 
-	//Read fieldinfo (dbffile is at correct offset because of readDBFHeader)
+	//Read fieldinfo
 	fields, err := readHeaderFields(dbffile)
 	if err != nil {
 		return nil, err
 	}
 
 	dbf := &DBF{
-		Header: header,
+		header: header,
 		f:      dbffile,
 		fields: fields,
 	}
@@ -188,7 +340,7 @@ func OpenFile(filename string) (*DBF, error) {
 		}
 
 		dbf.fptf = fptfile
-		dbf.FPTHeader = fptheader
+		dbf.fptheader = fptheader
 	}
 
 	return dbf, nil
@@ -212,7 +364,7 @@ func readFPTHeader(r io.ReadSeeker) (*FPTHeader, error) {
 	if _, err := r.Seek(0, 0); err != nil {
 		return nil, err
 	}
-	//Integers stored with the most significant byte first
+	//Integers in memo files are stored with the most significant byte first
 	err := binary.Read(r, binary.BigEndian, h)
 	if err != nil {
 		return nil, err
@@ -223,7 +375,7 @@ func readFPTHeader(r io.ReadSeeker) (*FPTHeader, error) {
 func validFileVersion(version byte) error {
 	switch version {
 	default:
-		return fmt.Errorf("Untested file version: %d (%x hex)", version, version)
+		return fmt.Errorf("Untested DBF file version: %d (%x hex)", version, version)
 	case 0x30, 0x31:
 		return nil
 	}
@@ -261,4 +413,45 @@ func readHeaderFields(r io.ReadSeeker) ([]FieldHeader, error) {
 		offset += FieldHeaderSize
 	}
 	return fields, nil
+}
+
+//Convert raw field data to the correct type
+func fieldDataToValue(raw []byte, fieldtype string, decimals uint8) (interface{}, error) {
+	//Not all fieldtypes have been implemented beacause we don't use them in our DBFs
+	//Extend this function if needed
+	switch fieldtype {
+	default:
+		return nil, fmt.Errorf("Unsupported fieldtype: %s", fieldtype)
+	case "C":
+		//TODO: Encoding
+		return string(raw), nil
+	case "M":
+		//TODO: Encoding
+		return "Memo: TODO", nil
+	case "I":
+		//I values are stored as numeric values
+		return rawToInt(raw)
+	case "N":
+		//N values are stored as string values
+		if decimals == 0 {
+			return strconv.ParseInt(strings.TrimSpace(string(raw)), 10, 32)
+		}
+		fallthrough //same as "F"
+	case "F":
+		//N values are stored as string values
+		return strconv.ParseFloat(strings.TrimSpace(string(raw)), 64)
+	case "D":
+		//D values are stored as string in format YYYYMMDD, convert to time.Time
+		return time.Parse("20060102", string(raw))
+	case "L":
+		return len(raw) > 0 && raw[0] == 0, nil
+	}
+}
+
+//Convert numeric binary byte value to int
+func rawToInt(raw []byte) (int32, error) {
+	var val int32 //The DBF size is 4 bytes so an int32 is big enough
+	buf := bytes.NewBuffer(raw)
+	err := binary.Read(buf, binary.LittleEndian, &val)
+	return val, err
 }
--- reader_test.go
+++ reader_test.go
@@ -1,22 +1,32 @@
 package dbf
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-//TODO: Create Test DBF files and commit them for more detailed tests
-//For now tests are a bit basic
 const (
-	DBF_PATH = "./testdbf/TEST.DBF"
+	TEST_DBF_PATH  = "./testdbf/TEST.DBF"
+	BENCH_DBF_PATH = "./testdbf/TEST.DBF" //For real benchmarks replace this with the path to a large DBF/FPT combo
 )
 
 var test_dbf *DBF
 
 func TestOpenFile(t *testing.T) {
 	var err error
-	test_dbf, err = OpenFile(DBF_PATH)
+	test_dbf, err = OpenFile(TEST_DBF_PATH)
 	if err != nil {
 		t.Fatal(err)
 	}
+}
 
+//Quick check if the first field matches
+func TestFieldHeader(t *testing.T) {
+	want := "{Name:[73 68 0 0 0 0 0 0 0 0 0] Type:73 Pos:1 Len:4 Decimals:0 Flags:0 Next:5 Step:1 Reserved:[0 0 0 0 0 0 0 78]}"
+	have := fmt.Sprintf("%+v", test_dbf.fields[0])
+	if have != want {
+		t.Errorf("First field from header does not match signature: Want %s, have %s", want, have)
+	}
 }
 
 //Test if the modified date of Stat() matches the header
@@ -27,7 +37,7 @@ func TestStat(t *testing.T) {
 		t.Fatal(err)
 	}
 	stat_mod := stat.ModTime()
-	hdr_mod := test_dbf.Header.Modified()
+	hdr_mod := test_dbf.header.Modified()
 	format := "20060102"
 	if stat_mod.Format(format) != hdr_mod.Format(format) {
 		t.Errorf("Modified date in header (%s) not equal to modified date in OS (%s)", hdr_mod.Format(format), stat_mod.Format(format))
@@ -37,8 +47,8 @@ func TestStat(t *testing.T) {
 //Tests if field headers have been parsed, fails if there are no fields
 func TestFieldNames(t *testing.T) {
 	fieldnames := test_dbf.FieldNames()
-	if len(fieldnames) == 0 {
-		t.Error("No fieldnames found")
+	if len(fieldnames) != 12 {
+		t.Errorf("Expected 12 fields, have %d", len(fieldnames))
 	}
 	t.Log(fieldnames)
 }
@@ -49,9 +59,10 @@ func TestFieldPos(t *testing.T) {
 		name string
 		pos  int
 	}{
-		{"NIVEAU", 0},
+		{"ID", 0},
+		{"NIVEAU", 1},
 		{"BLABLA", -1},
-		{"MELDING", 8},
+		{"FLOAT", 11},
 	}
 	for _, test := range cases {
 		pos := test_dbf.FieldPos(test.name)
@@ -61,9 +72,112 @@ func TestFieldPos(t *testing.T) {
 	}
 }
 
+func TestNumFields(t *testing.T) {
+	header := test_dbf.NumFields()
+	header_calc := test_dbf.Header().NumFields()
+	if header != header_calc {
+		t.Errorf("NumFields not equal. DBF NumFields: %d, DBF Header NumField: %d", header, header_calc)
+	}
+
+}
+
+func TestGoTo(t *testing.T) {
+	err := test_dbf.GoTo(0)
+	if err != nil {
+		t.Error(err)
+	}
+	if !test_dbf.BOF() {
+		t.Error("Expected to be at BOF")
+	}
+	err = test_dbf.GoTo(1)
+	if err != nil {
+		t.Error(err)
+	}
+	if test_dbf.EOF() {
+		t.Error("Did not expect to be at EOF")
+	}
+	err = test_dbf.GoTo(4)
+	if err != nil {
+		if err != ErrEOF {
+			t.Error(err)
+		}
+	}
+	if !test_dbf.EOF() {
+		t.Error("Expected to be at EOF")
+	}
+}
+
+func TestSkip(t *testing.T) {
+	test_dbf.GoTo(0)
+
+	err := test_dbf.Skip(1)
+	if err != nil {
+		t.Error(err)
+	}
+	if test_dbf.EOF() {
+		t.Error("Did not expect to be at EOF")
+	}
+	err = test_dbf.Skip(3)
+	if err != nil {
+		if err != ErrEOF {
+			t.Error(err)
+		}
+	}
+	if !test_dbf.EOF() {
+		t.Error("Expected to be at EOF")
+	}
+	err = test_dbf.Skip(-20)
+	if err != nil {
+		if err != ErrBOF {
+			t.Error(err)
+		}
+	}
+	if !test_dbf.BOF() {
+		t.Error("Expected to be at BOF")
+	}
+}
+
+//Tests a complete record read, reads the second record which is also deleted
+func TestRecord(t *testing.T) {
+	err := test_dbf.GoTo(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rec, err := test_dbf.Record()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(rec.data)
+}
+
 func TestClose(t *testing.T) {
 	err := test_dbf.Close()
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+//Benchmark for reading all records sequentially
+//Use a large DBF/FPT combo for more realistic results
+func BenchmarkReadRecords(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		err := func() error {
+			dbf, err := OpenFile(BENCH_DBF_PATH)
+			if err != nil {
+				return err
+			}
+			defer dbf.Close()
+			for i := uint32(0); i < dbf.NumRecords(); i++ {
+				_, err := dbf.Record()
+				if err != nil {
+					return err
+				}
+				dbf.Skip(1)
+			}
+			return nil
+		}()
+		if err != nil {
+			b.Fatal(err)
+		}
 	}
 }
